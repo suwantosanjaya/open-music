9cf6ca86b8c5b50fd9b2cb78774eb413e8a4cf4b
suwantosanjaya <suwantosanjaya@gmail.com>
2021-09-03 00:50:14 +0700

    Cek linter OK

--- migrations/1630573790714_CREATE-TABLE-open-music.js
+++ migrations/1630573790714_CREATE-TABLE-open-music.js
@@ -2,43 +2,43 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {
-    pgm.createTable('songs', {
-        id: {
-            type: 'VARCHAR(32)',
-            primaryKey: true,
-        },
-        title: {
-            type: 'TEXT',
-            notNull: true,
-        },
-        year: {
-            type: 'INTEGER',
-            notNull: true,
-        },
-        performer: {
-            type: 'VARCHAR(50)',
-            notNull: true,
-        },
-        genre: {
-            type: 'VARCHAR(20)',
-            notNull: true,
-        },
-        duration: {
-            type: 'INTEGER',
-            notNull: true,
-        },
-        inserted_at: {
-            type: 'VARCHAR(32)',
-            notNull: true,
-        },
-        updated_at: {
-            type: 'VARCHAR(32)',
-            notNull: true,
-        },
-    });
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(32)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(20)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    inserted_at: {
+      type: 'VARCHAR(32)',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'VARCHAR(32)',
+      notNull: true,
+    },
+  });
 };
 
-exports.down = pgm => {
-    pgm.dropTable('songs');
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
 };
